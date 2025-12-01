88cf78247e4cc6d02fbc42c97b175126edac1863
LiewYoung <liu3469449669@hotmail.com>
2025-12-01 11:35:32 +0800

    Hello world

--- src/main/java/top/liewyoung/network/agent.java
+++ src/main/java/top/liewyoung/network/agent.java
@@ -1,5 +1,35 @@
 package top.liewyoung.network;
-
+/*
+* @author: LiewYoung
+* @date: 25/12/1
+* @todo:
+*   add network for agent
+* */
 public class agent {
+    public String name;
+    public String personality;
 
+    public agent(String firstName,String lastName, String personality) {
+        this.name = firstName+" "+lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personality = personality;
+        this.sex = "男";
+    }
+    public agent(String firstName,String lastName, String personality,String sex) {
+        this.name = firstName+" "+lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personality = personality;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name + ", personality=" + personality;
+    }
+
+    protected String firstName;
+    protected String lastName;
+    protected String sex;
 }
